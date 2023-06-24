040226526627160612925e5fa2cd1fc76939fbd8
4lrick <alrick.rubio@epitech.eu>
2023-06-25 00:45:08 +0200

    Update .config/nvim/lua/keymaps.lua

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -34,23 +34,8 @@ keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
--- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
-
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-
 -- Delete current selection and paste while keeping original text
 keymap("v", "p", "P", opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Plugins
 
