a18364f80b95a10c04a0ab949452bff7a9e142dc
4lrick <alrick.rubio@epitech.eu>
2023-06-25 00:43:47 +0200

    Add .config/nvim/lua/user/noice.lua

---
+++ private_dot_config/nvim/lua/user/noice.lua
@@ -0,0 +1,33 @@
+local M = {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+}
+
+function M.config()
+	require("noice").setup({
+		lsp = {
+			hover = {
+				enabled = false,
+			},
+			signature = {
+				enabled = false,
+			},
+		},
+		notify = {
+			enabled = false,
+		},
+	})
+end
+
+return M
