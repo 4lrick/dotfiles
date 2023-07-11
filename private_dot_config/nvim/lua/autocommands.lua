50819c8810bc7b9eceab4c2053926f3fd18d1cc5
4lrick <alrick.rubio@epitech.eu>
2023-07-11 21:37:27 +0200

    Update .config/nvim/lua/autocommands.lua

--- private_dot_config/nvim/lua/autocommands.lua
+++ private_dot_config/nvim/lua/autocommands.lua
@@ -1,2 +1,17 @@
 -- Automatically close tab/vim when nvim-tree is the last window in the tab
 vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if line_count >= 5000 then
+      vim.cmd "IlluminatePauseBuf"
+    end
+  end,
+})
