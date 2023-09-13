1a5ff1bd0e193d2a7e4e5f297f890c263845a218
4lrick <alrick.rubio@epitech.eu>
2023-09-13 21:18:30 +0200

    Update .config/nvim/lua/autocommands.lua

--- private_dot_config/nvim/lua/autocommands.lua
+++ private_dot_config/nvim/lua/autocommands.lua
@@ -15,3 +15,8 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
