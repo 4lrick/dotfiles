21fcc82a20648573d4cf42e767219ee2b7fdabd4
4lrick <alrick.rubio@epitech.eu>
2023-10-05 11:22:36 +0200

    Update .config/polybar/scripts/temp.sh

--- private_dot_config/polybar/scripts/executable_temp.sh
+++ private_dot_config/polybar/scripts/executable_temp.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-notification_id=$$  # Utilise le PID comme ID de notification unique
-duration=5  # Durée de la boucle en secondes
-end_time=$((SECONDS + duration))  # Calcul de l'heure de fin
+notification_id=$$
+duration=5
 
-while [ $SECONDS -lt $end_time ]; do
+while [ $SECONDS -lt $duration ]; do
     temperature=$(sensors)
-    notify-send -r "$notification_id" "Température actuelle" "\n$temperature"
-    sleep 0.5  # Définissez ici l'intervalle de mise à jour en secondes
+    notify-send -r "$notification_id" "Current temperature" "\n$temperature"
+    sleep 0.5
 done
 
