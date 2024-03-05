a8875c4f4ed6926c2dbe78dd8c1a4cf86c69b478
4lrick <alrick.rubio@epitech.eu>
2024-03-05 19:52:33 +0100

    Update .config/nvim/lazy-lock.json Add .config/nvim/lua/user/lspsaga.lua Update .config/nvim/lua/user/nvim-lspconfig.lua

---
+++ private_dot_config/nvim/lua/user/lspsaga.lua
@@ -0,0 +1,30 @@
+local M = {
+	"nvimdev/lspsaga.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = "LspAttach",
+}
+
+function M.config()
+	require("lspsaga").setup({
+		code_action = {
+			show_server_name = true,
+		},
+		lightbulb = {
+			virtual_text = false,
+		},
+	})
+
+	local opts = { noremap = true, silent = true }
+	local keymap = vim.keymap.set
+
+	keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+	keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>", opts)
+	keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
+	keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+	keymap("n", "<leader>lr", "<cmd>Lspsaga rename<CR>", opts)
+end
+
+return M
