1e1cedc00309557acacdbb0ebb4b202d131c3600
4lrick <alrick.rubio@epitech.eu>
2024-12-22 14:22:00 -0500

    Update .config/nvim/lua/autocommands.lua

--- private_dot_config/nvim/lua/autocommands.lua
+++ private_dot_config/nvim/lua/autocommands.lua
@@ -7,8 +7,8 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ 'r', 'o' })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "r", "o" })
+	end,
 })
