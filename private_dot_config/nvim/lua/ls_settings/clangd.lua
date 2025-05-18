831bb373a35633f11eae3f2b8bdf186f5b67b62b
4lrick <alrick.rubio@epitech.eu>
2025-05-18 22:07:01 +0200

    Remove .config/nvim/lua/ls_settings

--- private_dot_config/nvim/lua/ls_settings/clangd.lua
+++
@@ -1,8 +0,0 @@
-return {
-	capabilities = require("cmp_nvim_lsp").default_capabilities(),
-	cmd = {
-		"clangd",
-		"--offset-encoding=utf-16",
-		"--clang-tidy",
-	},
-}
--- private_dot_config/nvim/lua/ls_settings/volar.lua
+++
@@ -1,8 +0,0 @@
-return {
-	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-	init_options = {
-		typescript = {
-			tsdk = "/home/alrick/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib",
-		},
-	},
-}
